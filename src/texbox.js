e64eb6561d6a2f8e6d8b10f312b2a06e1080446e
Dany Zatuchna <danzat@gmail.com>
2010-06-20 15:33:42 +0300

    Added horizontal lists to the TeX box-model and updated the test page

--- src/texbox.js
+++ src/texbox.js
@@ -39,3 +39,60 @@ md.Box = Class.define({
         }
     }
 });
+
+md.List = Class.define({
+    type: "List",
+    superclass: md.Box,
+    members: {
+        init: function (elements) {
+            this._super(0, 0, 0);
+            this.children = elements;
+        }
+    }
+});
+
+md.HList = Class.define({
+    type: "HList",
+    superclass: md.List,
+    members: {
+        init: function (elements) {
+            this._super(elements);
+            this.hpack();
+        },
+
+        hpack: function () {
+            var i;
+            var x = 0, d = 0, h = 0;
+            var p;
+            for (i = 0; i < this.children.length; i++) {
+                p = this.children[i];
+                x += p.width;
+                h = Math.max(h, p.height);
+                d = Math.max(d, p.depth);
+            }
+            this.height = h;
+            this.depth = d;
+            this.width = x;
+        },
+
+        render: function (ctx, x, y) {
+            var i;
+            var p;
+            ctx.save();
+            var px = x;
+            for (i = 0; i < this.children.length; i++) {
+                p = this.children[i];
+                p.render(ctx, px, y);
+                px += p.width;
+            }
+            ctx.strokeStyle = "blue";
+            ctx.strokeRect(x, y - this.height, this.width, this.height + this.depth);
+            ctx.strokeStyle = "red";
+            ctx.beginPath();
+            ctx.moveTo(x, y);
+            ctx.lineTo(x + this.width, y);
+            ctx.stroke();
+            ctx.restore();
+        }
+    }
+});
