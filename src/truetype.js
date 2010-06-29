7bc1c1e285222deff1bd3d89000458fd0624c7df
Dany Zatuchna <danzat@gmail.com>
2010-06-29 17:55:09 +0300

    Implementation of the parsing of the following tables: 1. head - general font information 2. maxp - some more font information :P 3. loca - glyph offset locations

--- src/truetype.js
+++ src/truetype.js
@@ -10,93 +10,155 @@ md.TTF = Class.define({
             var searchRange = f.UBInt16();
             var entrySelector = f.UBInt16();
             var rangeShift = f.UBInt16();
+            this.headers = {};
             for (var i = 0; i < numTables; i++) {
                 var tag = f.String(4);
                 var checksum = f.UBInt32();
                 var offset = f.UBInt32();
                 var length = f.UBInt32();
-                this[tag] = {checksum: checksum, offset: offset, length: length};
+                this.headers[tag] = {checksum: checksum, offset: offset, length: length, loaded: false};
             }
+            this['head'] = new md.tbl_head(this, this.file, this.headers['head']);
+            this['maxp'] = new md.tbl_maxp(this, this.file, this.headers['maxp']);
+            this['loca'] = new md.tbl_loca(this, this.file, this.headers['loca']);
+            this['glyf'] = new md.tbl_glyf(this, this.file, this.headers['glyf']);
+        }
+    }
+});
+
+md.tbl_head = Class.define({
+    members: {
+        init: function (font, file, header) {
+            this.font = font;
+            this.file = file;
+            this.header = header;
+            this.load();
         },
 
-        read_head: function () {
+        load: function () {
             var f = this.file;
-            f.seek(this['head'].offset, md.SEEK_SET);
-            this['head'].data = {};
-            this['head'].data.version = f.Fixed32();
-            this['head'].data.revision = f.Fixed32();
-            this['head'].data.checksum = f.UBInt32();
-            this['head'].data.magic = f.read(4);
-            this['head'].data.flags = f.UBInt16();
-            this['head'].data.unitsPerEm = f.UBInt16();
-            this['head'].data.created = f.read(8);
-            this['head'].data.modified = f.read(8);
-            this['head'].data.xMin = f.SBInt16();
-            this['head'].data.yMin = f.SBInt16();
-            this['head'].data.xMax = f.SBInt16();
-            this['head'].data.yMax = f.SBInt16();
-            this['head'].data.maxStyle = f.UBInt16();
-            this['head'].data.lowestRectPPEM = f.UBInt16();
-            this['head'].data.fontDirectionHint = f.SBInt16();
-            this['head'].data.indexToLocFormat = f.SBInt16();
-            this['head'].data.glyphDataFormat = f.SBInt16();
-        },
-        
-        read_maxp: function () {
-            var f = this.file;
-            f.seek(this['maxp'].offset, md.SEEK_SET);
-            this['maxp'].data = {};
-            this['maxp'].data.tableVersion = f.SBInt32();
-            this['maxp'].data.numGlyphs = f.UBInt16();
-            if (this['maxp'].data.tableVersion != 0x00005000) {
-                this['maxp'].data.maxPoints = f.UBInt16();
-                this['maxp'].data.maxContours = f.UBInt16();
-                this['maxp'].data.maxCompositePoints = f.UBInt16();
-                this['maxp'].data.maxCompositeContours = f.UBInt16();
-                this['maxp'].data.maxZones = f.UBInt16();
-                this['maxp'].data.maxTwilightPoints = f.UBInt16();
-                this['maxp'].data.maxStorage = f.UBInt16();
-                this['maxp'].data.maxFunctionDefs = f.UBInt16();
-                this['maxp'].data.maxInstructionDefs = f.UBInt16();
-                this['maxp'].data.maxStackElements = f.UBInt16();
-                this['maxp'].data.maxSizeOfInstruction = f.UBInt16();
-                this['maxp'].data.maxComponentElements = f.UBInt16();
-                this['maxp'].data.maxComponentDepth = f.UBInt16();
-            }
+            f.seek(this.header.offset, md.SEEK_SET);
+            this.version = f.Fixed32();
+            this.revision = f.Fixed32();
+            this.checksum = f.UBInt32();
+            this.magic = f.read(4);
+            this.flags = f.UBInt16();
+            this.unitsPerEm = f.UBInt16();
+            this.created = f.read(8);
+            this.modified = f.read(8);
+            this.xMin = f.SBInt16();
+            this.yMin = f.SBInt16();
+            this.xMax = f.SBInt16();
+            this.yMax = f.SBInt16();
+            this.maxStyle = f.UBInt16();
+            this.lowestRectPPEM = f.UBInt16();
+            this.fontDirectionHint = f.SBInt16();
+            this.indexToLocFormat = f.SBInt16();
+            this.glyphDataFormat = f.SBInt16();
+        }
+    }
+});
+
+md.tbl_maxp = Class.define({
+    members: {
+        init: function (font, file, header) {
+            this.font = font;
+            this.file = file;
+            this.header = header;
+            this.load();
         },
 
-        read_loca: function () {
-            var longFormat = this['head'].indexToLocFormat;
+        load: function () {
             var f = this.file;
-            f.seek(this['loca'].offset, md.SEEK_SET);
+            f.seek(this.header.offset, md.SEEK_SET);
+            this.tableVersion = f.SBInt32();
+            this.numGlyphs = f.UBInt16();
+            if (this.tableVersion != 0x00005000) {
+                this.maxPoints = f.UBInt16();
+                this.maxContours = f.UBInt16();
+                this.maxCompositePoints = f.UBInt16();
+                this.maxCompositeContours = f.UBInt16();
+                this.maxZones = f.UBInt16();
+                this.maxTwilightPoints = f.UBInt16();
+                this.maxStorage = f.UBInt16();
+                this.maxFunctionDefs = f.UBInt16();
+                this.maxInstructionDefs = f.UBInt16();
+                this.maxStackElements = f.UBInt16();
+                this.maxSizeOfInstruction = f.UBInt16();
+                this.maxComponentElements = f.UBInt16();
+                this.maxComponentDepth = f.UBInt16();
+            }
+        }
+    }
+});
+
+md.tbl_loca = Class.define({
+    members: {
+        init: function (font, file, header) {
+            this.font = font;
+            this.file = file;
+            this.header = header;
+            this.load();
+        },
+
+        load: function () {
+            var longFormat = this.font['head'].indexToLocFormat;
+            var f = this.file;
+            f.seek(this.header.offset, md.SEEK_SET);
             var locations = [];
             if (longFormat == 1) {
-                for (var i = 0; i < this['loca'].length / 4; i++) {
+                for (var i = 0; i < this.header.length / 4; i++) {
                     locations.push(f.SBInt32());
                 }
             } else {
-                for (var i = 0; i < this['loca'].length / 2; i++) {
+                for (var i = 0; i < this.header.length / 2; i++) {
                     locations.push(f.UBInt16());
                 }
             }
-            if (locations.length < (this['maxp'].data.numGlyphs + 1)) throw "Error: Corrupt 'loca' table or wrong 'numGlyphs' in table 'maxp'";
-            this['loca'].data = {locations: locations};
+            if (locations.length < (this.font['maxp'].numGlyphs + 1)) throw "Error: Corrupt 'loca' table or wrong 'numGlyphs' in table 'maxp'";
+            this.locations = locations;
+        }
+    }
+});
+
+md.tbl_glyf = Class.define({
+    members: {
+        init: function (font, file, header) {
+            this.font = font;
+            this.file = file;
+            this.header = header;
+            this.load();
         },
 
-        loadGlyphs: function () {
-            var f = this.file;
-            f.seek(this['glyf'].offset, md.SEEK_SET);
-            var numberOfContours = f.SBInt16();
-            var xMin = f.SBInt16();
-            var yMin = f.SBInt16();
-            var xMax = f.SBInt16();
-            var yMax = f.SBInt16();
-            console.debug(numberOfContours, xMin, yMin, xMax, yMax);
-            if (numberOfContours < 0) {
-                numberOfContours = -numberOfContours;
-                var flags = f.UBInt16();
-                var glyph_index = f.UBInt16();
+        load: function () {
+            var loca = this.font['loca'].locations;
+            this.glyphs = {};
+            var tbl_start = this.font.headers['glyf'].offset;
+            for (var i = 0; i < loca.length; i++) {
+                console.debug(i, loca[i] + tbl_start);
+                this.glyphs[i] = new md.Glyph(this.file, loca[i] + tbl_start);
             }
+        }
+    }
+});
+
+md.Glyph = Class.define({
+    members: {
+        init: function (file, offset) {
+            this.file = file;
+            this.offset = offset;
+            this.load();
+        },
+
+        load: function () {
+            var f = this.file;
+            f.seek(this.offset, md.SEEK_SET);
+            this.header = {};
+            this.header.numberOfContours = f.SBInt16();
+            this.header.xMin = f.SBInt16();
+            this.header.yMin = f.SBInt16();
+            this.header.xMax = f.SBInt16();
+            this.header.yMax = f.SBInt16();
         }
     }
 });
